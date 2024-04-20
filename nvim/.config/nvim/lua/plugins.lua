7d9507014772ea6388aace193aa5cff037984b86
takeyarise <>
2024-04-20 18:04:50 +0900

    udpate

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,19 +1,18 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
@@ -40,11 +39,11 @@ packer.init({
 
 -- Install your plugins here
 return packer.startup(function(use)
-	-- use({ "wbthomason/packer.nvim" })
+	use({ "wbthomason/packer.nvim" })
 	-- use({ "nvim-lua/plenary.nvim" }) -- Common utilities
 
 	-- Colorschemes
-	-- use({ "EdenEast/nightfox.nvim" }) -- Color scheme
+	use({ "EdenEast/nightfox.nvim" }) -- Color scheme
 
 	-- use({ "nvim-lualine/lualine.nvim" }) -- Statusline
 	-- use({ "windwp/nvim-autopairs" }) -- Autopairs, integrates with both cmp and treesitter
@@ -84,7 +83,7 @@ return packer.startup(function(use)
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
+	if packer_bootstrap then
 		require("packer").sync()
 	end
 end)
