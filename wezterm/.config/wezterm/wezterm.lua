d4b2a750ed47a8bb6a102d7503a5c5bff16344f0
takeyarise <>
2024-10-01 22:38:57 +0900

    update wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,17 +11,17 @@ end
 --]]
 
 return {
-	font = wezterm.font("HackGen Console NF", {weight="Regular", stretch="Normal", italic=false}),
+    font = wezterm.font("HackGen Console NF", {weight="Regular", stretch="Normal", italic=false}),
     -- font = wezterm.font("UDEV Gothic LG", {weight="Regular", stretch="Normal", style="Normal"}),
-	font_size = 12.0,
-	use_ime = true,
-	color_scheme = "nord",
+    font_size = 10.0,
+    use_ime = true,
+    color_scheme = "nord",
     audible_bell = "Disabled",
     visual_bell = {
         fade_in_duration_ms = 75,
         fade_out_duration_ms = 75,
         target = "CursorColor",
     },
-	-- window_background_opacity = 0.9,
-	adjust_window_size_when_changing_font_size = false,
+    -- window_background_opacity = 0.9,
+    adjust_window_size_when_changing_font_size = false,
 }
